2168c2f416f09f5df4e883f42900bc1862c1f699
Anthony Ngan <anthonyngan74@gmail.com>
2020-07-15 16:09:09 +1200

    completed website tutorial

---
+++ app.js
@@ -0,0 +1,16 @@
+'use strict'
+
+console.log('Here\'s a hidden message');
+
+// This is a single-line comment.
+
+/* Here's a
+multi-line
+comment. */
+
+// console.log('This code is no longer active')
+
+let today = new Date();
+let formatDate = today.toDateString();
+let selectElement = document.getElementById('date');
+selectElement.innerHTML = formatDate;
